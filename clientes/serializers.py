45b710b2209d8e31673b9ca5e8959c56359292ac
samuelbianch <samuelbianch38@gmail.com>
2023-01-31 16:52:56 -0300

    Adicionando validacao br do cpf

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,27 +1,23 @@
 from clientes.models import Cliente
 from rest_framework import serializers
+from clientes.validator import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos.")
-        return cpf
-    
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("O nome só aceita caracteres alfabéticos")
-        return nome
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 dígitos.")
-        return rg
-    
-    def validate_tel(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular deve ter o DD e o 9 antes do número, mais que 11 dígitos")
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "Este número de CPF não é válido"})
+
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "O nome só aceita caracteres alfabéticos"})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "O RG deve ter 9 dígitos."})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': "O celular deve seguir este modelo XX XXXXX-XXXX"})
+        
+        return data
