9e679fc13a49c4b07941ae4027a40c21fbbc74d2
samuelbianch <samuelbianch38@gmail.com>
2023-02-03 15:03:52 -0300

    Adicionando campos de busca e filtro

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend
@@ -7,5 +7,7 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']
+    filterset_fields = ['ativo']
